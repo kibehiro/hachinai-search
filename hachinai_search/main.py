548cea155329ea6523c25ae80615a6b500b175c9
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-01-19 00:51:41 +0900

    フォームに入力した内容を受け取れるように

--- hachinai_search/main.py
+++ hachinai_search/main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+import re
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('base.html')
+
+
+@app.route('/result')
+def search():
+    if request.method == 'GET':
+
+        card_name = re.split(r'\s+', request.args.get('card_name'))
+        cinderella_card_name = re.split(r'\s+', request.args.get('cinderella_card'))
+        skill_name = re.split(r'\s+', request.args.get('skill_name'))
+        ability_name = re.split(r'\s+', request.args.get('ability_name'))
+
+        rare_list = request.args.getlist('rare')
+        attribute_list = request.args.getlist('attribute')
+
+        return 'hoge'
+    else:
+        return redirect(url_for('/'))
 
 
 if __name__ == '__main__':
